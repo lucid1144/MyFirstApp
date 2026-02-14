699f2992ac57990b40f7681f82a2779d7e4aea23
lucid1144 <francischinedu155@gmail.com>
2026-02-14 01:12:33 +0100

    Add files via upload

---
+++ main.kts
@@ -0,0 +1,10 @@
+
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
